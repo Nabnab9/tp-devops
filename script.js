ca72322a789dd6ccc72c2881e1816707223c909b
alban.rousseau <alban.rousseau@o2.fr>
2025-03-14 23:43:30 +0100

    Enhance game feedback and improve user interaction
    Added detailed feedback messages with categorized results for guesses (TOO_HIGH, TOO_LOW, CORRECT, INVALID). Implemented an "Enter" key shortcut for guessing and refined styling with a hover effect on the "Deviner" button. Enhanced game logic for better readability and reset functionality renamed for clarity.

--- script.js
+++ script.js
@@ -1,36 +1,87 @@
+const TOO_HIGH = "TOO_HIGH";
+const TOO_LOW = "TOO_LOW";
+const CORRECT = "CORRECT";
+const INVALID = "INVALID";
+
 let randomNumber = 0;
 let attempts = 0;
-setGame();
 
-document.getElementById("btn").addEventListener("click", function() {
-    const userInput = document.getElementById("userInput").value;
-    const userGuess = parseInt(userInput);
-    attempts++;
+resetGame();
 
+document.getElementById("btn").addEventListener("click", play);
+document.getElementById("userInput").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        play();
+    }
+});
+
+function play() {
+    setMessageText("");
+
+    //Délai de 200ms pour montrer que le message est bien affiché
+    setTimeout(() => {
+        const userInput = document.getElementById("userInput").value;
+        const userGuess = parseInt(userInput);
+        attempts++;
+
+        let guessStatus = getGuessStatus(userGuess);
+
+        if (guessStatus.result === INVALID) {
+            setMessageText(guessStatus.message);
+        }
+
+        if (guessStatus.result === CORRECT) {
+            setMessageText(guessStatus.message);
+        }
+
+        if (guessStatus.result === TOO_HIGH) {
+            setMessageText(guessStatus.message);
+        }
+
+        if (guessStatus.result === TOO_LOW) {
+            setMessageText(guessStatus.message);
+        }
+    }, 200);
+
+}
+
+document.getElementById("btnRestart").addEventListener("click", function () {
+    resetGame();
+});
+
+function getGuessStatus(userGuess) {
     if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
-        setMessageText("Veuillez entrer un nombre valide entre 1 et 100.");
-        return;
+        return {
+            "result": INVALID,
+            "message": "Veuillez entrer un nombre valide entre 1 et 100."
+        };
     }
-
     if (userGuess === randomNumber) {
-        setMessageText(`Bravo ! Vous avez trouvé en ${attempts} essais.`);
-    } else if (userGuess > randomNumber) {
-        setMessageText("Trop haut ! Essayez encore.");
-    } else {
-        setMessageText("Trop bas ! Essayez encore.");
+        return {
+            "result": CORRECT,
+            "message": `Bravo ! Vous avez trouvé en ${attempts} essais.`
+        };
     }
-});
+    if (userGuess > randomNumber) {
+        return {
+            "result": TOO_HIGH,
+            "message": "Trop haut ! Essayez encore."
+        };
+    }
 
-document.getElementById("btnRestart").addEventListener("click", function() {
-    setGame();
-});
+    return {
+        "result": TOO_LOW,
+        "message": "Trop bas ! Essayez encore."
+    };
+}
 
 function setMessageText(buttonText) {
     document.getElementById("message").textContent = buttonText;
 }
 
-function setGame() {
+function resetGame() {
     randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(`Le nombre aléatoire est : ${randomNumber}`);
     attempts = 0;
     setMessageText("Nouveau jeu. Entrez un nombre entre 1 et 100.");
     document.getElementById("userInput").value = "";
